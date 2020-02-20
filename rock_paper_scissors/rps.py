59c57fcbb319edc80c3e41c1f6e32906a3719ba0
David Kuseh <davegru203@gmail.com>
2020-02-20 15:59:31 +0000

    complete rock,paper, scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,9 +3,23 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    result = []
+    rps = ['rock', 'paper', 'scissors']
 
+    # recursive function for adding lists by n
+    def rps_function(arr, n):
+        #base case
+        if n == 0:
+            return result.append(arr)
+        #adds string to arr
+        for i in rps:
+            #add each play to the array
+            rps_function(arr + [i], n-1)
+        
+    rps_function([], n)
+    return result
 
+      
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_plays = int(sys.argv[1])
